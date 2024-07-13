b3535f9bc80a6f94de354f913bffd26f54ce0d41
Rajeev Kumar <51359915+raj9226@users.noreply.github.com>
2024-07-13 23:40:41 +0530

    Add files via upload

---
+++ DockerWeb/src/test/java/com/raj/DockerWeb/DockerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.DockerWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
